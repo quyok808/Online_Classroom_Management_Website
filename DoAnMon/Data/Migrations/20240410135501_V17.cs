967cd25d7051d75cf90e6bd9add81aa71e51b1f4
CongQuy252 <nva2572003@gmail.com>
2024-04-13 23:11:06 +0700

    Fix database

--- DoAnMon/Data/Migrations/20240410135501_V17.cs
+++ DoAnMon/Data/Migrations/20240410135501_V17.cs
@@ -19,28 +19,10 @@ namespace DoAnMon.Data.Migrations
                 oldType: "nvarchar(max)",
                 oldNullable: true);
 
-            migrationBuilder.AddColumn<string>(
-                name: "CustomRoleId",
-                table: "classroomDetail",
-                type: "nvarchar(450)",
-                nullable: true);
-
-            migrationBuilder.CreateIndex(
-                name: "IX_classroomDetail_CustomRoleId",
-                table: "classroomDetail",
-                column: "CustomRoleId");
-
             migrationBuilder.CreateIndex(
                 name: "IX_classroomDetail_RoleId",
                 table: "classroomDetail",
                 column: "RoleId");
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_classroomDetail_AspNetRoles_CustomRoleId",
-                table: "classroomDetail",
-                column: "CustomRoleId",
-                principalTable: "AspNetRoles",
-                principalColumn: "Id");
 
             migrationBuilder.AddForeignKey(
                 name: "FK_classroomDetail_AspNetRoles_RoleId",
@@ -53,24 +35,13 @@ namespace DoAnMon.Data.Migrations
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropForeignKey(
-                name: "FK_classroomDetail_AspNetRoles_CustomRoleId",
-                table: "classroomDetail");
 
             migrationBuilder.DropForeignKey(
                 name: "FK_classroomDetail_AspNetRoles_RoleId",
                 table: "classroomDetail");
 
             migrationBuilder.DropIndex(
-                name: "IX_classroomDetail_CustomRoleId",
-                table: "classroomDetail");
-
-            migrationBuilder.DropIndex(
                 name: "IX_classroomDetail_RoleId",
-                table: "classroomDetail");
-
-            migrationBuilder.DropColumn(
-                name: "CustomRoleId",
                 table: "classroomDetail");
 
             migrationBuilder.AlterColumn<string>(
