ac386f698c6bbe6d8d35617e54c2fd93c72e4750
CongQuy252 <nva2572003@gmail.com>
2024-04-16 01:11:25 +0700

    đã chat được nhưng chua luu vao database

---
+++ DoAnMon/Data/Migrations/20240415152745_V33.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DoAnMon.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class V33 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.CreateTable(
+                name: "messages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Noidung = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Time = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ClassRoomId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_messages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_messages_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_messages_classRooms_ClassRoomId",
+                        column: x => x.ClassRoomId,
+                        principalTable: "classRooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_messages_ClassRoomId",
+                table: "messages",
+                column: "ClassRoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_messages_UserId",
+                table: "messages",
+                column: "UserId");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ DoAnMon/SignalR/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace DoAnMon.SignalR
+{
+	public class ChatHub : Hub
+	{
+		public async Task SendMessage(string user, string message)
+		{
+			// Gửi tin nhắn đến tất cả các kết nối
+			await Clients.All.SendAsync("ReceiveMessage", user, message);
+		}
+	}
+}
