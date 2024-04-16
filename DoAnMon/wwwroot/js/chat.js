c6ba6a18897097788dbfec3947873dab0b76c5f8
CongQuy252 <nva2572003@gmail.com>
2024-04-17 01:39:46 +0700

    Xong Phần thảo luận trong classroom_detail

--- DoAnMon/wwwroot/js/chat.js
+++ DoAnMon/wwwroot/js/chat.js
@@ -5,23 +5,6 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 //Disable the send button until connection is established.
 document.getElementById("sendButton").disabled = true;
 
-connection.on("ReceiveMessage", function (user, message) {
-    var li = document.createElement("li");
-    document.getElementById("messagesList").appendChild(li);
-    // We can assign user-supplied strings to an element's textContent because it
-    // is not interpreted as markup. If you're assigning in any other way, you
-    // should be aware of possible script injection concerns.
-    const currentTime = new Date();
-    const year = currentTime.getFullYear(); // Năm (ví dụ: 2024)
-    const month = currentTime.getMonth() + 1; // Tháng (từ 0 - 11, cần cộng thêm 1)
-    const day = currentTime.getDate(); // Ngày trong tháng
-    const hours = currentTime.getHours(); // Giờ (24-giờ)
-    const minutes = currentTime.getMinutes(); // Phút
-    const seconds = currentTime.getSeconds(); // Giây
-    const formatTime = `${hours}:${minutes}:${seconds} - ${day}/${month}/${year}`;
-    li.textContent = `${user} (${formatTime}) says ${message}`;
-});
-
 connection.start().then(function () {
     document.getElementById("sendButton").disabled = false;
 }).catch(function (err) {
@@ -38,16 +21,19 @@ document.getElementById("sendButton").addEventListener("click", function (event)
     });
 
     event.preventDefault();
-
-    // Clear và focus vào trường tin nhắn
-    //document.getElementById("messageInput").value = "";
-    //document.getElementById("messageInput").focus();
 });
 
 document.getElementById("messageInput").addEventListener("keydown", function (event) {
     if (event.key == 'Enter' && !event.shiftKey) {
-        // Bắn sự kiện click cho nút gửi khi người dùng nhấn Enter
-        document.getElementById("sendButton").click();
+        var user = document.getElementById("userInput").value;
+        var message = document.getElementById("messageInput").value;
+        var classId = document.getElementById("classId").value;
+
+        connection.invoke("SendMessage", user, message).catch(function (err) {
+            return console.error(err.toString());
+        });
+
+        event.preventDefault();
         event.preventDefault();
     }
 });
