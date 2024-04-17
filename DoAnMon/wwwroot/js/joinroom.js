626338f43683c1b9b3e14f692f7854ea17ac31ca
CongQuy252 <nva2572003@gmail.com>
2024-04-18 00:24:50 +0700

    Update them class

---
+++ DoAnMon/wwwroot/js/joinroom.js
@@ -0,0 +1,26 @@
+﻿
+function joinRoom(event) {
+    console.log('Clicked!');
+    event.preventDefault();
+    var roomid = prompt("Nhập mã phòng:");
+    if (roomid !== null && roomid.trim() !== "") {
+        saveUserIdToDatabase(roomid);
+    }
+}
+
+function saveUserIdToDatabase(roomid) {
+    $.ajax({
+        type: "POST",
+        url: "/ClassRooms/joinClass",
+        data: {
+            roomid: roomid
+        },
+        success: function () {
+
+            alert("Bạn " + @user?.Name + " đã tham gia vào phòng học có mã" + roomid);
+        },
+        error: function (error) {
+            console.error(error);
+        }
+    });
+}
