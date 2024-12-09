2916ded5aed18ca58b862f92294d338e8c2d1c94
CongQuy252 <quyok25203@gmail.com>
2024-12-10 01:35:25 +0700

    Xong chức năng nộp bài tập theo nhóm

---
+++ DoAnMon/Migrations/20241209152342_them_LoaiBT_Table_BaiTap.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DoAnMon.Migrations
+{
+    /// <inheritdoc />
+    public partial class them_LoaiBT_Table_BaiTap : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FileFormat",
+                table: "baiTaps",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Loaibt",
+                table: "baiTaps",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Loaibt",
+                table: "baiTaps");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FileFormat",
+                table: "baiTaps",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
